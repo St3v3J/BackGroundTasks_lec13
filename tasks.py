b1e476f42c131927fee9a7ea9c5b8de0b2da92e0
Steven Johnson <stevendgjohnson@gmail.com>
2020-09-10 18:41:25 -0500

    first commit

--- tasks.py
+++ tasks.py
@@ -9,19 +9,6 @@ huey = RedisHuey(url=os.getenv('REDIS_URL'))
 
 
 # task
-@huey.task(retries=5, retry_delay=5)
-def get_random_num():
-    print("This is a task to get a random number")
-    num = random.randint(1, 3)
-    print("Random number is {}".format(num))
-
-    if num == 1:  # if number is 1, the task was successful
-        return True
-    else:  # if number is not 1, raise an error (so that we see how a failed task looks like and what happens next)
-        raise Exception("Error in the worker... :(")
-
-
-# task
 @huey.task(retries=10, retry_delay=600)
 def send_email_task(receiver_email, subject, text):
     sender_email = os.getenv("MY_SENDER_EMAIL")  # Your website's official email address
