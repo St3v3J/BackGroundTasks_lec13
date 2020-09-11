e61176e4347e02d223c1dc33197377593c66c0f3
Steven Johnson <stevendgjohnson@gmail.com>
2020-09-11 15:13:22 -0500

    first commit

--- utils/email_helper.py
+++ utils/email_helper.py
@@ -3,8 +3,10 @@ from tasks import send_email_task
 
 
 def send_email(receiver_email, subject, text):
+
     if os.getenv('REDIS_URL'):  # basically, if the web app is on Heroku, call the "send email" task
         send_email_task(receiver_email, subject, text)
+
     else:  # but if you're on localhost, just simulate sending email in the console
         print("You are on localhost, so the email will not be really sent.")
         print("---------------EMAIL MESSAGE----------------")
